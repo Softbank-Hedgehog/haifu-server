93505a23c0bf123c8301c40cf5188472e1318146
Jaehoon-So <jhspacelover@naver.com>
2025-11-10 02:58:52 +0900

    feat: health API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
-from app.routers import users
+from app.routers import users, health
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,3 +24,4 @@ app.add_middleware(
 )
 
 app.include_router(users.router)
+app.include_router(health.router)
